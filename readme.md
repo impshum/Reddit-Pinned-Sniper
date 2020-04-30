03d2da6ac42b3d9f603adf74a4b264e998cf243d
impshum <impshum@impshum.co.uk>
2020-04-30 13:37:27 +0100

    up we go

---
+++ readme.md
@@ -0,0 +1,22 @@
+## Reddit Pinned Sniper
+
+Reposts or crossposts pinned submissions from chosen subreddits
+
+### Instructions
+
+-   Install requirements `pip install -r requirements.txt`
+-   Create Reddit (script) app at https://www.reddit.com/prefs/apps/ and get keys
+-   Edit conf.ini with your details
+-   Run it `python run.py`
+
+#### Settings Info
+
+-   `main_subreddit` - Subreddit to post to
+-   `target_subreddits` - Subreddits to search for pinned submissions (comma separated)
+-   `crosspost` - Crosspost or repost (on = crosspost)
+-   `send_replies` - Send comment replies to inbox from new submission
+-   `test_mode` - Runs the bot without posting
+
+#### Tip
+
+BTC - 1AYSiE7mhR9XshtS4mU2rRoAGxN8wSo4tK
---
+++ run.py
@@ -0,0 +1,86 @@
+import praw
+from time import sleep
+import schedule
+import configparser
+import pickledb
+from requests import get
+from requests.exceptions import ConnectionError
+
+
+def wait_until_online(timeout, slumber):
+    offline = 1
+    t = 0
+    while offline:
+        try:
+            r = get('https://google.com', timeout=timeout).status_code
+        except ConnectionError:
+            r = None
+        if r == 200:
+            offline = 0
+        else:
+            t += 1
+            if t > 3:
+                quit()
+            else:
+                print('BOT OFFLINE')
+                sleep(slumber)
+
+
+def do_db(db, id, sub):
+    if not db.exists(id):
+        db.set(id, sub)
+        db.dump()
+        return True
+
+
+def sniper(reddit, main_subreddit, target_subreddits, send_replies, crosspost, test_mode, db):
+    wait_until_online(10, 3)
+    for target_subreddit in target_subreddits:
+        for submission in reddit.subreddit(target_subreddit).new(limit=None):
+            if submission.stickied:
+                title = submission.title
+                selftext = submission.selftext
+                if not test_mode:
+                    if do_db(db, submission.id, target_subreddit):
+                        if crosspost:
+                            submission.crosspost(
+                                subreddit=main_subreddit, send_replies=send_replies)
+                        else:
+                            reddit.subreddit(main_subreddit).submit(
+                                title, selftext=selftext, send_replies=send_replies)
+                        print(f'r/{target_subreddit} → {title[0:80]}')
+
+
+def main():
+    db = pickledb.load('history.db', False)
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+    main_subreddit = config['SETTINGS']['main_subreddit']
+    target_subreddits = config['SETTINGS']['target_subreddits'].split(',')
+    send_replies = config['SETTINGS'].getboolean('send_replies')
+    crosspost = config['SETTINGS'].getboolean('crosspost')
+    test_mode = config['SETTINGS'].getboolean('test_mode')
+    reddit = praw.Reddit(
+        username=config['REDDIT']['reddit_user'],
+        password=config['REDDIT']['reddit_pass'],
+        client_id=config['REDDIT']['reddit_client_id'],
+        client_secret=config['REDDIT']['reddit_client_secret'],
+        user_agent='Pinned Sniper (by u/impshum)'
+    )
+
+    if test_mode:
+        print('\nTEST MODE\n')
+
+    sniper(reddit, main_subreddit, target_subreddits,
+           send_replies, crosspost, test_mode, db)
+
+    schedule.every().hour.do(sniper, reddit=reddit, main_subreddit=main_subreddit, target_subreddits=target_subreddits,
+                             send_replies=send_replies, crosspost=crosspost, test_mode=test_mode, db=db)
+
+    while True:
+        schedule.run_pending()
+        sleep(1)
+
+
+if __name__ == '__main__':
+    main()
